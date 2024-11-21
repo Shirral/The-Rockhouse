160635a6db6fb28fa233e4ad68e46c956b71b207
Shirral <132143164+Shirral@users.noreply.github.com>
2024-11-21 19:45:24 +0000

    Add rocks app and index.html template; move the templates folder to the root project folder

--- the_rockhouse/settings.py
+++ the_rockhouse/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +48,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # custom apps
+    'rocks',
 ]
 
 SITE_ID = 1
@@ -73,7 +81,10 @@ ROOT_URLCONF = 'the_rockhouse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
