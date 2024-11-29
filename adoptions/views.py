017e5d6b027361c2852058af67524e468f0b3b3e
Shirral <132143164+Shirral@users.noreply.github.com>
2024-11-29 17:46:21 +0000

    Update Stripe's card element; update requirements.txt
    Wire up the Stripe's card element to accept payments.

--- adoptions/views.py
+++ adoptions/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from rocks.models import Rock
 from .forms import AdoptionForm
+import stripe
 
 
 def adoption_confirm(request, rock_id):
@@ -18,14 +19,24 @@ def adoption_confirm(request, rock_id):
 def adoption_form(request, rock_id):
 
     rock = get_object_or_404(Rock, pk=rock_id)
+    cost = rock.price
+    stripe_cost = round(cost * 100)
     adoption_form = AdoptionForm()
-    stripe_public_key = stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_cost,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    print(intent)
 
     context = {
         'rock': rock,
         'adoption_form': adoption_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test client secret'
+        'client_secret': intent.client_secret
     }
 
     return render(request, 'adoptions/adoption-form.html', context)
