85e1d1dc850dd842d46568e48d917482f142a9c4
Shirral <132143164+Shirral@users.noreply.github.com>
2024-11-30 15:23:28 +0000

    Update the adpotion_form view
    Wire up the form to save the information about the adoption to the database.

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.conf import settings
+from django.contrib.auth.models import User
 from rocks.models import Rock
+from .models import RockAdoption
 from .forms import AdoptionForm
 import stripe
 
@@ -18,25 +21,51 @@ def adoption_confirm(request, rock_id):
 
 def adoption_form(request, rock_id):
 
-    rock = get_object_or_404(Rock, pk=rock_id)
-    cost = rock.price
-    stripe_cost = round(cost * 100)
-    adoption_form = AdoptionForm()
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_cost,
-        currency=settings.STRIPE_CURRENCY
-    )
+    rock = get_object_or_404(Rock, pk=rock_id)
+    cost = rock.price
 
-    print(intent)
+    if request.method == 'POST':
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'address1': request.POST['address1'],
+            'address2': request.POST['address2'],
+            'postcode': request.POST['postcode'],
+            'town': request.POST['town'],
+            'country': request.POST['country'],
+        }
 
-    context = {
-        'rock': rock,
-        'adoption_form': adoption_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret
-    }
+        adoption_form = AdoptionForm(form_data)
+        if adoption_form.is_valid():
+            adoption = adoption_form.save(commit=False)
 
-    return render(request, 'adoptions/adoption-form.html', context)
+            adoption.rock = rock
+            adoption.user = request.user
+            adoption.cost = cost
+            adoption.is_paid = False  # updated later with webhook?
+            adoption.stripe_id = ""  # updated later with webhook?
+            # adoption.adoption_number = uuid.uuid4().hex.upper()  # id
+            adoption.adoption_number = adoption._generate_adoption_number()
+
+            adoption.save()
+
+        return redirect(reverse('adoption-success', args=[adoption.id]))
+
+    else:
+        stripe_cost = round(cost * 100)
+        adoption_form = AdoptionForm()
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_cost,
+            currency=settings.STRIPE_CURRENCY
+        )
+
+        context = {
+            'rock': rock,
+            'adoption_form': adoption_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret
+        }
+
+        return render(request, 'adoptions/adoption-form.html', context)
