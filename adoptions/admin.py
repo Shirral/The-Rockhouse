8effdbb570d9ee10f4a6df39ba7c3d0cce2ca0c6
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-01 17:05:44 +0000

    Update admin.py
    Update the display of the adoptions in the admin panel.

--- adoptions/admin.py
+++ adoptions/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.RockAdoption)
+class RockAdoptionAdmin(admin.ModelAdmin):
+    readonly_fields = ('stripe_id', 'adoption_number')
+    list_display = ('rock', 'user', 'date', 'adoption_number')
+    ordering = ('-date',)
+
+admin.site.register(models.RockAdoption, RockAdoptionAdmin)
