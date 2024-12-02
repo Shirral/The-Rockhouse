416d218f9aedffcfe37beb5e7fc5de5c81c0620b
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-02 21:30:01 +0000

    Prepare the app for Heroku deployment
    Create and load the fixture file with the rock data; create a Procfile; update settings.py and requirements.txt

--- the_rockhouse/settings.py
+++ the_rockhouse/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = 'django-insecure-(nia$=v)3drf3^n9%=3b^d+j!9o4wb=y%1)h_ycq)tmayv94g-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'the-rockhouse-781674c0484c.herokuapp.com',
+    'localhost'
+]
 
 
 # Application definition
@@ -64,8 +67,8 @@ SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
-#ACCOUNT_EMAIL_VERIFICATION = 'none'
+# ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -127,12 +130,17 @@ WSGI_APPLICATION = 'the_rockhouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
