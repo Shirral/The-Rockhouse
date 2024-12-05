05a9a308a33dc96c7f153379304335835ac9ffc6
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-05 17:52:33 +0000

    Add user profile template and its view  & url

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rocks.models import Rock
+from adoptions.models import RockAdoption
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def profile(request, username):
+
+    user = get_object_or_404(User, username=username)
+    user_id = user.id
+    rocks = Rock.objects.filter(owner=user_id)
+    rock_number = len(rocks)
+    # adoption_date = only that from RockAdoption? Or the above, too?
+
+    context = {
+        'user': user,
+        'rocks': rocks,
+        'rock_number': rock_number
+    }
+
+    return render(request, 'userprofile/profile.html', context)
