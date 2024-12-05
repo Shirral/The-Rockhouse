19b494e841eb48ce77aef026ccff529c322e90e3
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-05 19:55:57 +0000

    Update user profile page

--- userprofile/views.py
+++ userprofile/views.py
@@ -8,14 +8,13 @@ def profile(request, username):
 
     user = get_object_or_404(User, username__iexact=username)
     user_id = user.id
-    rocks = Rock.objects.filter(owner=user_id)
-    rock_number = len(rocks)
-    # adoption_date = only that from RockAdoption? Or the above, too?
+    rock_adoption = RockAdoption.objects.filter(user=user_id)
+    rock_number = len(rock_adoption)
 
     context = {
         'user': user,
-        'rocks': rocks,
-        'rock_number': rock_number
+        'rock_number': rock_number,
+        'rock_adoption': rock_adoption
     }
 
     return render(request, 'userprofile/profile.html', context)
