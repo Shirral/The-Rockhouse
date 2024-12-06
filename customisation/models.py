694c3873d07c2c10963fcd151ef8c578d832b47c
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-06 23:19:34 +0000

    Add Accessories model, render the accessory images in the template

--- customisation/models.py
+++ customisation/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Accessories(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    colour = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    rock_shape = models.CharField(max_length=50, null=True, blank=True)
+    image = models.ImageField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
--- customisation/views.py
+++ customisation/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from rocks.models import Rock
+from .models import Accessories
 
 
 def customisation(request, rock_id):
 
     rock = get_object_or_404(Rock, pk=rock_id)
+    accessories = Accessories.objects.filter(type="accessory")
+    frames = Accessories.objects.filter(type="frame")
 
     context = {
-        'rock': rock
+        'rock': rock,
+        'accessories': accessories,
+        'frames': frames
     }
 
     return render(request, 'customisation/customisation.html', context)
