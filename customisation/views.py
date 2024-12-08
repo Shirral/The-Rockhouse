a6d053a7b1cd9c9ad9e1f52bc3f1d23a84cea1df
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-08 16:35:23 +0000

    Add POST method handling to the customisation view

--- customisation/views.py
+++ customisation/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from rocks.models import Rock
 from .models import Accessories
 
@@ -8,6 +8,24 @@ def customisation(request, rock_id):
     rock = get_object_or_404(Rock, pk=rock_id)
     accessories = Accessories.objects.filter(type="accessory")
     frames = Accessories.objects.filter(type="frame")
+
+    if request.method == "POST":
+
+        selected_accessories = request.POST.getlist('accessory')
+        selected_frame = request.POST.get('frame')
+
+        print(selected_accessories)
+        print(selected_frame)
+
+        customisation_json = {
+            'accessories': selected_accessories,
+            'frame': selected_frame if selected_frame != 'frame_none' else None,
+        }
+
+        rock.accessories = customisation_json
+        rock.save()
+
+        return redirect('customisation', rock_id=rock.id)
 
     context = {
         'rock': rock,
