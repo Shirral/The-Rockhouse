412035623b253a0a26c7faa5911b1360deca6973
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-08 10:06:13 +0000

    Add removing accessories when the checkbox is clicked again
    Add removing accesories when the checkbox is marked unchecked again.

--- customisation/static/customisation/js/customisation.js
+++ customisation/static/customisation/js/customisation.js
@@ -3,16 +3,26 @@ const accessories = $(".accessory-option");
 const frames = $(".frame-option");
 
 $("#accessory-options").on("change", ".accessory-option", function () {
-    const accessoryImage = $(this).next().clone().appendTo("#accessories-layer"); // clone image & add to accessory div
-    accessoryImage.css({
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "auto",
-        zIndex: 2,
-    });
+    let accessoryId = $(this).val();
+    
+    let accessoryImage = $(this).next().clone().attr("data-accessory-id", accessoryId) // clone image & add to accessory div & copy its id from value
 
- 
+    if (this.checked) {
+        accessoryImage.css({
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "auto",
+            zIndex: 2,
+        });
+
+        accessoryImage.appendTo("#accessories-layer");
+    } else {
+        
+        $(`#accessories-layer [data-accessory-id="${accessoryId}"]`).remove();
+        // accessoryImage.remove();
+    }
+
 });
 
