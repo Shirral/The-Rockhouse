109889c451ac97f92703e3145252f4fe64777867
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-08 15:29:51 +0000

    Add frame selection
    Selected frame now displays on the rock image.

--- customisation/static/customisation/js/customisation.js
+++ customisation/static/customisation/js/customisation.js
@@ -21,8 +21,30 @@ $("#accessory-options").on("change", ".accessory-option", function () {
     } else {
         
         $(`#accessories-layer [data-accessory-id="${accessoryId}"]`).remove();
-        // accessoryImage.remove();
     }
 
 });
 
+$("#frame-options").on("change", ".frame-option", function () {
+    let frameId = $(this).val();
+
+    $("#frames-layer").empty();
+
+    // if the -no frame- option is selected, end here - do not add anything else
+    if (frameId == "frame_none"){
+        return;
+    }
+
+    let frameImage = $(this).next().clone().attr("data-frame-id", frameId) // clone image & add to accessory div & copy its id from value
+    frameImage.css({
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "auto",
+        zIndex: 3,
+    });
+    
+    frameImage.appendTo("#frames-layer");
+
+});
