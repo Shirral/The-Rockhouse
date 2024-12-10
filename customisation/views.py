e47cb285ef4bddadd8b27bd2cba0907cf6c6d860
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-10 17:02:21 +0000

    Add adding user notes to the customisation page

--- customisation/views.py
+++ customisation/views.py
@@ -11,6 +11,7 @@ def customisation(request, rock_id):
     accessories = Accessories.objects.filter(type="accessory")
     frames = Accessories.objects.filter(type="frame")
     user = request.user
+    user_notes = rock.user_notes
 
     if user != rock.owner:
         messages.warning(request, "Only the owner of this rock can customise it. If you are the owner of this rock, please log in using the account you used to adopt it!")
@@ -20,6 +21,7 @@ def customisation(request, rock_id):
 
         selected_accessories = request.POST.getlist('accessory')
         selected_frame = request.POST.get('frame')
+        user_notes = request.POST.get('usernotes')
 
         selected_accessories = [int(x) for x in selected_accessories if x.isdigit()]
 
@@ -29,6 +31,7 @@ def customisation(request, rock_id):
         }
 
         rock.accessories = customisation_json
+        rock.user_notes = user_notes
         rock.save()
 
         return redirect('customisation', rock_id=rock.id)
@@ -48,7 +51,8 @@ def customisation(request, rock_id):
         'accessories': accessories,
         'frames': frames,
         'selected_frame': selected_frame,
-        'selected_accessories': selected_accessories
+        'selected_accessories': selected_accessories,
+        'user_notes': user_notes
     }
 
     return render(request, 'customisation/customisation.html', context)
