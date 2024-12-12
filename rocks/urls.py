7dd48d57ff1901866b6d80532b3ed03e80185348
Shirral <132143164+Shirral@users.noreply.github.com>
2024-12-12 17:36:29 +0000

    Add rock-edit template

--- rocks/urls.py
+++ rocks/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.adoptions, name='adoptions'),
     path('rockprofile/<int:rock_id>/', views.rockprofile, name='rockprofile'),
+    path('rock/edit/<int:rock_id>/', views.rock_edit, name='rock_edit'),
 ]
--- rocks/views.py
+++ rocks/views.py
@@ -44,3 +44,30 @@ def rockprofile(request, rock_id):
     }
 
     return render(request, 'rocks/rockprofile.html', context)
+
+
+def rock_edit(request, rock_id):
+
+    rock = get_object_or_404(Rock, pk=rock_id)
+    accessories = Accessories.objects.filter(type="accessory")
+    frames = Accessories.objects.filter(type="frame")
+
+    if rock.accessories and rock.accessories != "None":
+        selected_accessories = rock.accessories['accessories']
+        if rock.accessories['frame']:
+            selected_frame = int(rock.accessories['frame'])
+        else:
+            selected_frame = 'None'
+    else:
+        selected_accessories = []
+        selected_frame = 'None'
+
+    context = {
+        'rock': rock,
+        'accessories': accessories,
+        'frames': frames,
+        'selected_frame': selected_frame,
+        'selected_accessories': selected_accessories
+    }
+
+    return render(request, 'rocks/rock-edit.html', context)
